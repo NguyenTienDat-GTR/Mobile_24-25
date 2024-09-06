8bdc73a8e54fa40b0cf470de7212e432fb48d9df
H51M14\Student <Student@gmail.com>
2024-09-06 08:52:28 +0700

    add file JS of week03

---
+++ week03/week03.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+
+const YourApp = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'flex-start',
+        backgroundImage:
+          'linear-gradient(to bottom, #C7F4F6, #D1F4F6, #E5F4F5, #00CCF9',
+      }}>
+      <View
+        style={{
+          flex: 2,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Image
+          source={'https://picsum.photos/200'}
+          style={{ width: '5rem', height: '5rem' }}
+        />
+      </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+        }}>
+        <Text style={{ textAlign: 'center', fontWeight: 'bold' }}>
+          We will help you to grow your business using online server
+        </Text>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'space-around',
+          alignItems: 'center',
+          flexDirection: 'row',
+        }}>
+        <button style={{ backgroundColor: 'yellow', border: 'none' }}>
+          LOGIN
+        </button>
+        <button style={{ backgroundColor: 'yellow', border: 'none' }}>
+          SIGN UP
+        </button>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          fontFamily: 'Roboto',
+          fontSize: '1rem',
+          fontWeight: 'bold',
+          justifyContent: 'flex-end',
+        }}>
+        <text style={{ marginBottom: 20 }}>HOW WE WORK?</text>
+      </View>
+    </View>
+  );
+};
+
+export default YourApp;
